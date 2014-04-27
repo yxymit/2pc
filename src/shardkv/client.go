9fe280391356cc99d924535a41d028a934fbad0a
Xiangyao Yu <yxy@mit.edu>
2014-04-27 16:33:59 -0400

    [2pc] cleanup common.go

--- src/shardkv/client.go
+++ src/shardkv/client.go
@@ -5,85 +5,33 @@ import "net/rpc"
 import "time"
 import "sync"
 import "fmt"
-import "crypto/rand"
-import "math/big"
 
 type Clerk struct {
   mu sync.Mutex // one RPC at a time
   config shardmaster.Config
   groups map[int64][]string // gid -> servers[]
-  me int64
+  txnid int
+  me int
   rpcid int
 }
 
-func nrand() int64 {
-  max := big.NewInt(int64(1) << 62)
-  bigx, _ := rand.Int(rand.Reader, max)
-  x := bigx.Int64()
-  return x
-}
-
-func MakeClerk(groups map[int64][]string) *Clerk {
+func MakeClerk(me int, groups map[int64][]string) *Clerk {
   ck := new(Clerk)
   ck.groups = groups
-  ck.me = nrand() 
+  ck.txnid = 0
+  ck.me = me
   ck.rpcid = 0
   return ck
 }
 
-//
-// call() sends an RPC to the rpcname handler on server srv
-// with arguments args, waits for the reply, and leaves the
-// reply in reply. the reply argument should be a pointer
-// to a reply structure.
-//
-// the return value is true if the server responded, and false
-// if call() was not able to contact the server. in particular,
-// the reply's contents are only valid if call() returned true.
-//
-// you should assume that call() will time out and return an
-// error after a while if it doesn't get a reply from the server.
-//
-// please use call() to send all RPCs, in client.go and server.go.
-// please don't change this function.
-//
-func call(srv string, rpcname string,
-          args interface{}, reply interface{}) bool {
-  c, errx := rpc.Dial("unix", srv)
-  if errx != nil {
-    return false
-  }
-  defer c.Close()
-    
-  err := c.Call(rpcname, args, reply)
-  if err == nil {
-    return true
-  }
-
-  fmt.Println(err)
-  return false
-}
-
-//
-// which shard is a key in?
-// please use this function,
-// and please do not change it.
-//
-func key2shard(key string) int {
-  shard := 0
-  if len(key) > 0 {
-    shard = int(key[0])
-  }
-  shard %= shardmaster.NShards
-  return shard
-}
-
-// statically map a shard to a group
-func shard2group(shard int) int {
-  return shard % 10;
-}
 
 func RunTxn(reqs []TxnArgs) {
+  var args InsOpArgs 
+  args.txn_id = txnid * 100 + ck.me
+  for _, req := range reqs {
+     
+     args.txn.PushBack() 
+  }
   type InsOpArgs struct {
     txn_id int
     txn list.List
